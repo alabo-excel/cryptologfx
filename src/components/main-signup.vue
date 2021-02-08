76b50aa6e3188eb6959e9130aef2cc8ec6c469fb
alabo-excel <iamalaboexcel@gmail.com>
2021-02-08 17:23:19 +0100

    responsive commit

--- src/components/main-signup.vue
+++ src/components/main-signup.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="hello flex justify-center">
-    <div class="text-xs my-20">
+  <div class="flex justify-center">
+    <div class="text-xs my-20 pl-8 lg:pl-0">
       <h1 class="td-blue text-2xl font-extrabold text-center m-8">
         Create <span class="tl-blue">Account</span>
       </h1>
@@ -32,15 +32,15 @@
         <input
           type="password"
           class="pl-6 border-2 lg:m-2 my-3 p-3 lg:w-64 w-80 rounded-md border-blue"
-          placeholder="E-mail"
-        /><p class="pt-5 pr-12">Select Country</p>
+          placeholder="United States"
+        /><p class="lg:pt-5 pr-12 lg:mr-3">Select Country</p>
         <input
           type="text"
           class="pl-6 border-2 lg:m-2 p-3 my-3 lg:w-96 w-80 rounded-md border-blue"
           placeholder="Confirm Password"
         />
       </div>
-      <div class="flex ml-2 m-6">
+      <div class="flex lg:w-auto w-80 ml-2 m-6">
           <input type="checkbox" class="p-4 mr-6 w-6 h-6">
           <p class="pt-1">I want to receive CryptoLogFX Newsletters with best deals and offers</p>
       </div>
@@ -48,7 +48,7 @@
         <input
           type="button"
           value="SIGN UP"
-          class="m-2 p-3 rounded-md bg-l-blue text-white lg:w-big w-80"
+          class="lg:m-2 p-3 rounded-md bg-l-blue text-white diff"
           placeholder="Password"
         />
       </div>
@@ -67,4 +67,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.diff{
+  width: 98%;
+}
+@media(max-width: 670px){
+  .diff{
+    width: 20rem;
+  }
+}
 </style>
